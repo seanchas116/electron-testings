94f052fddc3137423558eb3f3c7b0c27dc9fa7e5
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-02 22:19:00 +0900

    Add modal corner example

---
+++ modal/index.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require("electron")
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({width: 600, height: 400})
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+  const modalWindow = new BrowserWindow({width: 100, height: 100, parent: mainWindow, modal: true, show: false})
+  modalWindow.loadURL('file://' + __dirname + '/modal.html')
+
+  // rounded corner appears
+  setTimeout(() => {
+    modalWindow.show()
+  }, 1000)
+
+  // rounded corner does not appear
+  // modalWindow.show()
+})
