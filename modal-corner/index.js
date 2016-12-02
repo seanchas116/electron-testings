c90d1a10df36b0c16bcca366d05a0a8d0cb97294
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-02 22:30:47 +0900

    Smaller window

--- modal-corner/index.js
+++ modal-corner/index.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require("electron")
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({width: 600, height: 400})
+  const mainWindow = new BrowserWindow({width: 200, height: 200})
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   const modalWindow = new BrowserWindow({width: 100, height: 100, parent: mainWindow, modal: true, show: false})
