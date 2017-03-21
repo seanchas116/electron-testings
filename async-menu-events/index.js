0547d319dd42f248e24cfd51af179a3f03917ed2
Ryohei Ikegami <iofg2100@gmail.com>
2017-03-21 18:45:53 +0900

    Test on renderer

--- async-menu-events/index.js
+++ async-menu-events/index.js
@@ -4,14 +4,13 @@ app.on('ready', () => {
   const mainWindow = new BrowserWindow({width: 600, height: 400})
   mainWindow.loadURL('file://' + __dirname + '/index.html')
   mainWindow.openDevTools()
-  mainWindow.webContents.on('dom-ready', () => {
-    console.log("ready to show")
-    const menu = Menu.buildFromTemplate([
-      {label: '1'}, {label: '2'}, {label: '3'}
-    ])
-    menu.popup(mainWindow, {x: 100, y: 100, async: true})
-    setTimeout(() => {
-      console.log("timed out")
-    }, 100)
-  })
+  // mainWindow.webContents.on('dom-ready', () => {
+  //   const menu = Menu.buildFromTemplate([
+  //     {label: '1'}, {label: '2'}, {label: '3'}
+  //   ])
+  //   menu.popup(mainWindow, {x: 100, y: 100, async: true})
+  //   setTimeout(() => {
+  //     console.log("timed out")
+  //   }, 100)
+  // })
 })
